49ca50e4bd8ece29894fb8c3635595caf38920d9
Thibaut <thibautjombart@gmail.com>
2015-10-27 12:05:08 +0000

    fixed MH code

--- R/mh.R
+++ R/mh.R
@@ -7,40 +7,41 @@
 #'
 #' @param x a dataset to compute the log-density on (likelihood, posterior, etc.)
 #' @param f a function computing log-densities
-#' @param param a list of named parameters passed on to 'f'
+#' @param param a list of named parameters passed on to 'f'; parameters must be given their initial values
+#' @param to.move a logical or an integer vector indicating which parts of 'param' should be moved in the MCMC
 #' @param n a number of iterations for the MCMC
 #'
 #' @importFrom coda mcmc
-metro <- function(f, param=list(x=0), n=1e4,
+metro <- function(f, param=list(x=1), to.move=TRUE, n=1e4,
                   sd=1){
     ## FIND WHICH ARGUMENTS NEED TO MOVE ##
-    toMove <- which(sapply(param, is.numeric))
-    nParam <- length(toMove)
-
+    nParam <- length(param)
+    if(nParam<1) stop("At least one parameter is needed to define the domain of f")
+    if(!is.null(param)) to.move <- rep(to.move, length=nParam)
+    if(is.logical(to.move)) to.move <- which(to.move)
+    param.names <- names(param)
+    
     ## DEFINE OVERAL DENSITY COMPUTATION ##
     logdens <- function(param){
         return(sum(do.call(f, param)))
     }
 
     ## pre-generate unif random variates
-    RAND <- log(runif(length(toMove)*n))
+    RAND <- log(runif(length(to.move)*n))
     COUNTER <- 1
 
     ## build output
     out.log <- double(n)
-    out.param <- matrix(double(n*nParam),ncol=nParam)
-    colnames(out.param) <- names(toMove)
-
-    ## init param
-    out.param[1,] <- rnorm(nParam, mean=0, sd=20)
+    out.param <- list()
+    out.param[1:n] <- param
 
     ## for each iteration
     for(i in 2:n){
         ## move each param
-        param <- out.param[i-1,]
-        for(j in 1:nParam){
+        param <- out.param[i-1]
+        for(j in to.move){
             new <- param
-            new[j] <- rnorm(1, mean=new[j],sd=sd)
+            new[[j]] <- rnorm(1, mean=new[[j]],sd=sd)
             logratio <- logdens(new) - logdens(param)
 
             ## accept/reject
@@ -48,12 +49,16 @@ metro <- function(f, param=list(x=0), n=1e4,
             COUNTER <- COUNTER+1
         }
         ## store param
-        out.param[i,] <- param
+        out.param[i] <- param
+
+        ## store log dens
+        out.log[i] <- logdens(param)
     }
 
     ## SHAPE OUTPUT ##
+    out.param <- matrix(unlist(out.param), byrow=TRUE, ncol=nParam)
+    colnames(out.param) <- param.names
     out <- cbind(out.log, out.param)
-    names(out)[1] <- "logdens"
     out <- mcmc(out, start=1, end=n, thin=1)
     return(out)
 } # end metro
